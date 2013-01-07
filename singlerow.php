722ae4f114392b1b6ec58e851f042b396f92c93a
tanyataylor <tatiana@blueacorn.com>
2013-01-07 14:42:02 -0500

    Step 6 included

--- singlerow.php
+++ singlerow.php
@@ -1,12 +1,11 @@
 <?php
-
-//var_dump($_POST);
 error_reporting(E_ALL|E_STRICT);
-ini_set("display_errors", 1);
+ini_set('display_errprs',1);
+
+$sku = $_GET['sku'];
 
 $xml = json_decode(json_encode(simpleXML_load_file('/var/www/magento/app/etc/local.xml','SimpleXMLElement', LIBXML_NOCDATA)),true);
 //var_dump($xml);
-
 $contents = $xml['global']['resources']['default_setup']['connection'];
 $connect_attributes = array_slice($contents,0,4);
 $link = mysql_connect($connect_attributes['host'],$connect_attributes['username'],$connect_attributes['password']);
@@ -18,17 +17,6 @@ $db_selected = mysql_select_db($connect_attributes['dbname'],$link);
 if (!$db_selected){
     die('Can\'t use db : ' . mysql_error());
 }
-if(isset($_POST['sortoption'])){
-    $sortoption = $_POST["sortoption"];
-}
-else $sortoption = "sku";
-
-if(isset($_POST['sortorder'])){
-    $sortorder = $_POST["sortorder"];
-}
-else $sortorder = "ASC";
-
-
 
 $sql = "SELECT catalog_product_entity.sku, catalog_product_entity_varchar.value, core_website.name
 FROM catalog_product_entity
@@ -38,51 +26,24 @@ JOIN catalog_product_website
 ON catalog_product_website.product_id = catalog_product_entity.entity_id
 JOIN core_website
 ON catalog_product_website.website_id = core_website.website_id
-WHERE catalog_product_entity_varchar.attribute_id = 96
-ORDER BY {$sortoption}" . " {$sortorder}";
-
-if((isset($_POST['limit'])) AND $_POST['limit'] > 0){
-    $limit = $_POST['limit'];
-    $sql .= " limit 0, {$limit}";
-} else{
-    $sql .= " limit 0, 30";
-}
+WHERE catalog_product_entity_varchar.attribute_id = 96 AND catalog_product_entity.sku = '" .$_GET['sku']. "'";
 
 $result = mysql_query($sql);
 if(!$result){
     die("Invalid query: " . mysql_error());
 }
-//var_dump($result);
-//if(!isset($_POST['submit'])){
 ?>
-
-<html>
-<head>
-    <title>SORT THE RESULTS</title>
-</head>
-<body>
-<form name="form1" method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Enter Limit:<input type="text" size="12" maxlength="3" name="limit"><br /><br />
-    Sort Option: SKU<input type="radio" value="sku" name="sortoption">
-    Value<input type="radio" value="value" name="sortoption"><br />
-    Sort Order: ASC <input type="radio" value="ASC" name="sortorder">
-    DSC<input type="radio" value="desc" name="sortorder"><br /><br />
-    <input type="submit" value="Submit!" size="12" name="submit"><br />
-    </form>
-</body>
-</html>
-
 <table>
     <tr><th>SKU</th><th>Value</th><th>Website_Name</th></tr>
 <?php //if(!isset($_POST['submit'])){ /* display the form if submit is not set */} else { ?>
 
     <?php
     while ($row = mysql_fetch_assoc($result)){
-        echo "<tr><td>" . '<a href="singlerow.php?sku=' .$row['sku'] . '">' . $row['sku'] . '</a>'.
-            "</td><td>"  .$row['value'] .
+        echo "<tr><td>" .$row['sku'] .
+            "</td><td>" .$row['value'] .
             "</td><td>" . $row['name'] .
             "</td></tr>";
-        }
+    }
 
 //var_dump($row);
 ?>
